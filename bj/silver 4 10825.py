808cfefc7561ca28a4ac40238b848276f398fc57
kimjunwoo0330 <zoo990330@gmail.com>
2022-07-24 13:19:02 +0900

    bj

--- bj/silver 4 10825.py
+++ bj/silver 4 10825.py
@@ -1,9 +1,10 @@
 import sys
 data=[]
+
 for _ in range(int(input())):
     data.append(sys.stdin.readline().split())
 
 data=sorted(data,key=lambda x : (-int(x[1]),int(x[2]),-int(x[3]),x[0]))
-print(data)
+
 for i in data:
     print(i[0])
